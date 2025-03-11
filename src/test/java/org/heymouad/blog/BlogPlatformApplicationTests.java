6785885ae695cefc1effe54c8f08c7f38623ac4f
devcom33 <mouad.elb06@gmail.com>
2025-03-11 06:55:07 +0000

    first commit of my blog backend app

---
+++ src/test/java/org/heymouad/blog/BlogPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package org.heymouad.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
