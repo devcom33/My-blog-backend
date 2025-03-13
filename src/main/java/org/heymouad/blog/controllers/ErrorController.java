971aa17fff27172960fa74da0b45a15008529c3b
devcom33 <mouad.elb06@gmail.com>
2025-03-13 15:43:09 +0000

    add errorHandiling controller methods handleexception handleIllegalArgumentException

--- src/main/java/org/heymouad/blog/controllers/ErrorController.java
+++ src/main/java/org/heymouad/blog/controllers/ErrorController.java
@@ -1,4 +1,36 @@
 package org.heymouad.blog.controllers;
 
+
+import lombok.extern.slf4j.Slf4j;
+import org.heymouad.blog.domain.dtos.ApiErrorResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ControllerAdvice
+@Slf4j
 public class ErrorController {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiErrorResponse> handleException(Exception ex) {
+        log.error("Caught exception", ex);
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
+                .message("An unexpected Error Occured")
+                .build();
+
+        return new ResponseEntity<>(error, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ApiErrorResponse error = ApiErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(ex.getMessage())
+                .build();
+        return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+    }
 }
